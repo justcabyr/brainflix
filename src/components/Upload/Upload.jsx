15bb4fb6a30fa3e452d23fbf8fe98903f4099d15
AbdulKabir Sulaiman <justcabyr@gmail.com>
2024-08-25 01:07:07 -0230

    configured upload routing

---
+++ src/components/Upload/Upload.jsx
@@ -0,0 +1,16 @@
+import './Upload.scss';
+
+import Header from '../Header/Header';
+
+function Upload() {
+  return (
+    <>
+      <Header />
+      <div>
+        <h2>Upload sommething</h2>
+      </div>
+    </>
+  );
+}
+
+export default Upload;
