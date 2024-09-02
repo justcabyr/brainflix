71494c9afddc1ce0e014e882546001a0c6ea25f1
AbdulKabir Sulaiman <justcabyr@gmail.com>
2024-09-02 18:20:18 -0230

    added controller to add video

--- server/src/models/videos.js
+++ server/src/models/videos.js
@@ -3,39 +3,32 @@ import { uuid } from 'uuidv4';
 import fs from 'fs';
 
 export const getVideos = () => {
-  return videoList[1];
+  return videoList;
 };
 
 export const getVideoById = (id) => {
   return videoList.find((video) => video.id === id);
 };
 
-let newVideo = {
-  id: uuid(),
-  title: 'Entering the false mood',
-  channel: 'Mohan Muruge',
-  image: 'http://localhost:8080/images/Upload-video-preview.jpg',
-  description:
-    'Through rare footage, interviews, and historical accounts, this video paints a vivid picture of the indomitable human spirit, pushing the boundaries of endurance and scientific discovery in one of the most remote places on the planet. Brace yourself for an awe-inspiring journey into the heart of Antarctica, where exploration meets the untamed beauty of the frozen frontier',
-  views: '0',
-  likes: '0',
-  duration: `${Math.floor(Math.random() * 20)}:${Math.floor(Math.random() * 60)}`,
-  video: 'http://localhost:8080/videos/BrainStation_Sample_Video.mp4',
-  timestamp: Date.now(),
-  comments: [],
-};
-
 const path = './data/video-details.json';
 
-export const addVideo = () => {
+export const addVideo = (newVideo) => {
   // Read the JSON file
   const data = fs.readFileSync(path, 'utf8');
   const jsonData = JSON.parse(data);
 
   // Add the new video object to the array
-  jsonData.push(newVideo);
+  newVideo.id = uuid();
+  newVideo.image = 'http://localhost:8088/images/sample-image.jpg';
+  newVideo.channel = 'Mohan Muruge';
+  newVideo.views = '0';
+  newVideo.likes = '0';
+  newVideo.duration = `${Math.floor(Math.random() * 20)}:${Math.floor(Math.random() * 60)}`;
+  newVideo.video = 'http://localhost:8088/videos/brainstation-sample-video.mp4';
+  newVideo.timestamp = Date.now();
+  newVideo.comments = [];
 
-  console.log('here');
+  jsonData.push(newVideo);
 
   // Write the updated data back to the JSON file
   fs.writeFileSync(path, JSON.stringify(jsonData, null, 2), 'utf8');
